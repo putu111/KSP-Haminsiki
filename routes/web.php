0422374dd2e2242acdd6c675225c8273e0a64d8c
denis1517 <43105134+denis1517@users.noreply.github.com>
2020-05-10 12:49:49 +0800

    Update middleware route

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('locale/{locale}', function ($locale){
+    Session::put('locale', $locale);
+    return redirect()->back();
+});
 
 Route::get('/sign-in', 'UserController@index');
 Route::get('/', 'UserController@home');
